7042db06153f8cda68ad612860f2dd822d62112c
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-19 14:49:17 -0700

    Updated the reviewRoute.js file with DELETE a review by id fucntion

--- controller/api/reviewRoute.js
+++ controller/api/reviewRoute.js
@@ -50,5 +50,26 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
-
+// DELETE a review by id (withAuth middleware for authentication)
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const reviewData = await Review.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!reviewData) {
+        res.status(404).json({ message: 'No review found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(reviewData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
   
