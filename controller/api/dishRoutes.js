03a0e0a24f8c962ff745d6528c98025d3889f72d
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-19 14:28:58 -0700

    Created the utils/auth.js as a middleware protection

--- controller/api/dishRoutes.js
+++ controller/api/dishRoutes.js
@@ -33,4 +33,6 @@ router.get('/:id', async (req, res) => {
 });
 
 
+
+
 module.exports = router;
---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+const authMiddleware = (req, res, next) => {
+    // Check if the user is logged in
+    if (!req.session.logged_in) {
+      // If not logged in, redirect to the login page
+      return res.redirect('/login');
+    }
+    
+    // If the user is logged in, proceed to the next middleware or route handler
+    next();
+  };
+  
+  module.exports = authMiddleware;
