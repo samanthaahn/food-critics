0d59cbbcc9f9798ca1fe4220298ad8c82f78ab8c
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-19 13:18:03 -0700

    fixed index in model

--- models/index.js
+++ models/index.js
@@ -2,12 +2,18 @@ const Dish = require('./Dish');
 const User = require('./User');
 const Review = require('./Review');
 
-User.hasMany(Review, {
+Dish.belongsTo(User, {
     foreignKey: 'user_id'
+});
+
+Dish.hasMany(Review, {
+    foreignKey: 'dish_id'
 });
 
 Review.belongsTo(User, {
     foreignKey: 'user_id'
 });
+
+
 
 module.exports = { Dish, User, Review };
