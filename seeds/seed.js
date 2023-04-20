a994260ea09c66e125ecdb5b4ae7d95e76a755c9
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-20 13:13:31 -0700

    added seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,25 @@
 const sequelize = require('../config/connection');
 const { User,  Dish, Review } = require('../models');
 
+const userData = require('./userSeed.json');
+const dishData = require('./dishSeed.json');
+const reviewData = require('./reviewSeed.json');
+
+const seedDatabase = async () => {
+await sequelize.sync({ force: true });
+
+const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+});
+
+for (const dish of dishData) {
+    await Dish.create({
+        ...dish,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+}
+process.exit(0);
+};
+
+seedDatabase();
